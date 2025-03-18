8b57d9eba647274727989e6c1e4f9b2575929777
Arsen Islyamov <arsenislyam@yandex.ru>
2025-03-18 20:10:13 +0300

    Solve Task with Mutex

---
+++ DiningPhilosophers/Philosopher.cs
@@ -0,0 +1,54 @@
+﻿namespace DiningPhilosophers;
+
+public class Philosopher(int id, Mutex leftFork, Mutex rightFork)
+{
+    public void Run()
+    {
+        while (true)
+        {
+            Think();
+            Eat();
+        }
+    }
+
+    private void Think()
+    {
+        Console.WriteLine($"Философ {id} думает.");
+        Thread.Sleep(new Random().Next(1000, 2000));
+    }
+
+    private void Eat()
+    {
+        Console.WriteLine($"Философ {id} пытается начать кушать");
+        //ему нужны обе сразу
+        bool hasLeftFork = false;
+        bool hasRightFork = false;
+
+        try
+        {
+            hasLeftFork = leftFork.WaitOne(1000); //берет в левую
+            if (hasLeftFork)
+            {
+                hasRightFork = rightFork.WaitOne(1000); //берет в правую
+                if (hasRightFork)
+                {
+                    Console.WriteLine($"Философ {id} кушает");
+                    Thread.Sleep(new Random().Next(1000, 2000)); //кушает столько
+                }
+            }
+        }
+        finally
+        {
+            if (hasRightFork)
+            {
+                rightFork.ReleaseMutex();
+            }
+            if (hasLeftFork)
+            {
+                leftFork.ReleaseMutex();
+            }
+        }
+
+        Console.WriteLine($"Философ {id} закончил кушать");
+    }
+}
--- DiningPhilosophers/Program.cs
+++ DiningPhilosophers/Program.cs
@@ -1,2 +1,24 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Threading;
+using DiningPhilosophers;
+
+public static class Program
+{
+    static void Main(string[] args)
+    {
+        int numPhilosophers = 5;
+        Philosopher[] philosophers = new Philosopher[numPhilosophers];
+        Mutex[] forks = new Mutex[numPhilosophers];
+
+        for (int i = 0; i < numPhilosophers; i++)
+        {
+            forks[i] = new Mutex();
+        }
+
+        for (int i = 0; i < numPhilosophers; i++)
+        {
+            philosophers[i] = new Philosopher(i, forks[i], forks[(i + 1) % numPhilosophers]);
+            new Thread(philosophers[i].Run).Start();
+        }
+    }
+}
